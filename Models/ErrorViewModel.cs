fc0dbaa496a9b1934a9923be1393d75f64184294
gilrsantana <g.r.santana@hotmail.com>
2021-08-16 05:46:57 -0300

    first commit

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace aulas_desafio21dias_mvc.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
